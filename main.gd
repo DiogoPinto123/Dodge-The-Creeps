93638cf1690ba064e1f35f03c471e9a91a5ecd60
Diogo Pinto <m54062@alunos.uevora.pt>
2023-07-10 13:21:02 +0100

    Implemented HUD and added sound

--- main.gd
+++ main.gd
@@ -6,12 +6,18 @@ var score
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Music.play()
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
@@ -40,10 +46,8 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
-	
-func _ready():
-	new_game()
